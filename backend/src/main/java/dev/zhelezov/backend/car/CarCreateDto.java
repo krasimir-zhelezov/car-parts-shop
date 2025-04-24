572d8b75ee448e1dbad56b067480775fbcf108a0
krasimir-zhelezov <krasizh07@gmail.com>
2025-04-24 16:07:08 +0300

    backend(feat): added create car dto

---
+++ backend/src/main/java/dev/zhelezov/backend/car/CarCreateDto.java
@@ -0,0 +1,40 @@
+package dev.zhelezov.backend.car;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class CarCreateDto {
+    @NotBlank
+    private String brand;
+    
+    @NotBlank
+    private String model;
+
+    @NotBlank
+    private int productionYear;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+
+}
