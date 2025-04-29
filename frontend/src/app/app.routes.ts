5f7215259c5bfd3be49f09a0ac56bb1c6af15aa7
krasimir-zhelezov <krasizh07@gmail.com>
2025-04-29 12:30:23 +0300

    frontend(feat): car-list component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CarListComponent } from './car-list/car-list.component';
 
 export const routes: Routes = [
-    { path: 'dashboard', component: DashboardComponent }
+    { path: 'dashboard', component: DashboardComponent, data: { isDashboard: true },
+        children: [
+            { path: 'cars', component: CarListComponent }
+        ]
+     }
 ];
--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { HlmButtonModule } from '@spartan-ng/ui-button-helm';
+import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [HlmButtonModule],
+  imports: [HlmButtonModule, RouterOutlet, NgIf],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -11,4 +14,11 @@ export class DashboardComponent {
   totalCars: number = 234;
   totalManufacturers: number = 178;
   totalParts: number = 2556;
+
+  get isDashboardRoute() {
+    return this.route.snapshot.data?.['isDashboard'] || 
+         this.route.snapshot.firstChild?.data?.['isDashboard'];
+  }
+
+  constructor(private route: ActivatedRoute) {}
 }
