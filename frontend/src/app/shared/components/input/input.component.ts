c6bd1119f34064b194285d8838679457f0ba197f
krasimir-zhelezov <krasizh07@gmail.com>
2025-05-13 22:51:18 +0300

    feat(frontend): input component

---
+++ frontend/src/app/shared/components/input/input.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'ui-input',
+  imports: [],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css'
+})
+export class InputComponent {
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
+  @Input() value: string = '';
+  @Output() valueChange = new EventEmitter<string>();
+
+  onInput(event: Event) {
+    this.value = (event.target as HTMLInputElement).value;
+    this.valueChange.emit(this.value);
+  }
+}
--- frontend/src/app/shop/shop.component.ts
+++ frontend/src/app/shop/shop.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { InputComponent } from '../shared/components/input/input.component';
 
 @Component({
   selector: 'app-shop',
-  imports: [],
   templateUrl: './shop.component.html',
-  styleUrl: './shop.component.css'
+  styleUrl: './shop.component.css',
+  imports: [InputComponent]
 })
 export class ShopComponent {
+  brands = ['brand1', 'brand2', 'brand3']
 
+  selectedBrand: string = '';
+
+  onBrandSelected(brand: string) {
+    this.selectedBrand = brand;
+  }
 }
