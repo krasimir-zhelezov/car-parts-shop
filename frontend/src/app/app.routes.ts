73bde1c073777d27e130c3f41d3c809e1d3aede3
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-01 21:32:30 +0300

    feat(frontend): parts base component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ShopComponent } from './shop/shop.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CarsComponent } from './dashboard/cars/cars.component';
+import { PartsComponent } from './dashboard/parts/parts.component';
 
 export const routes: Routes = [
     { path: "", component: ShopComponent },
     { path: "dashboard", component: DashboardComponent, children: [
-        { path: "cars", component: CarsComponent }
+        { path: "cars", component: CarsComponent },
+        { path: "parts", component: PartsComponent }
     ] }
 ];
---
+++ frontend/src/app/dashboard/parts/parts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parts',
+  imports: [],
+  templateUrl: './parts.component.html',
+  styleUrl: './parts.component.css'
+})
+export class PartsComponent {
+
+}
