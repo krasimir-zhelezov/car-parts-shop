16deaf760b4977240f93917de72c9d979c60b3f1
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-01 21:43:17 +0300

    feat(frontend): car & parts model + get all parts service method

---
+++ frontend/src/app/dashboard/parts/parts.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Part } from '../../models/part.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PartsService {
+
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getAllParts(): Observable<Part[]> {
+    return this.http.get<Part[]>(`${this.baseUrl}/parts/`);
+  }
+}
---
+++ frontend/src/app/models/part.model.ts
@@ -0,0 +1,11 @@
+import { Car } from "./car.model";
+
+export interface Part {
+  id: string;
+  name: string;
+  code: string;
+  category: string;
+  supportedCars: Car[];
+  buyPrice: number;
+  sellPrice: number;
+}
