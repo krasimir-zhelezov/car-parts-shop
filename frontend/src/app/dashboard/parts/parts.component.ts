282f9db5d88a538b07f6ec97e58c0b4c38b4990d
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-01 21:53:21 +0300

    feat(frontend): parts table

--- frontend/src/app/dashboard/parts/parts.component.ts
+++ frontend/src/app/dashboard/parts/parts.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Part } from '../../models/part.model';
+import { PartsService } from './parts.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-parts',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './parts.component.html',
   styleUrl: './parts.component.css'
 })
-export class PartsComponent {
+export class PartsComponent implements OnInit {
+  parts: Part[] = [];
 
+  constructor(private partsService: PartsService) { }
+
+  ngOnInit(): void {
+    this.loadParts();
+  }
+
+  private loadParts(): void {
+    this.partsService.getAllParts().subscribe({
+      next: (parts) => {
+        this.parts = parts;
+      }
+    })
+  }
 }
