a08d0e0326d8b2e739363239621c6dc4bd3775f9
krasimir-zhelezov <krasizh07@gmail.com>
2025-06-02 21:07:48 +0300

    feat(frontend): add part form

--- frontend/src/app/dashboard/parts/parts.component.ts
+++ frontend/src/app/dashboard/parts/parts.component.ts
@@ -2,15 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { Part } from '../../models/part.model';
 import { PartsService } from './parts.service';
 import { NgFor } from '@angular/common';
+import { ButtonComponent } from "../../shared/components/button/button.component";
+import { ModalComponent } from '../../shared/components/modal/modal.component';
+import { InputComponent } from '../../shared/components/input/input.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-parts',
-  imports: [NgFor],
+  imports: [NgFor, ButtonComponent, ModalComponent, InputComponent, FormsModule],
   templateUrl: './parts.component.html',
   styleUrl: './parts.component.css'
 })
 export class PartsComponent implements OnInit {
   parts: Part[] = [];
+  isAddPartModalOpen: boolean = false;
+  
+  partName: string = '';
+  partCode: string = '';
+  partCategory: string = '';
+  //supportedCars: Car[];
+  partBuyPrice: number = 0;
+  partSellPrice: number = 0;
+
 
   constructor(private partsService: PartsService) { }
 
@@ -24,5 +37,17 @@ export class PartsComponent implements OnInit {
         this.parts = parts;
       }
     })
+  }
+
+  addPart(): void {
+    console.log(`Adding part ${this.partName}`);
+  }
+
+  openAddPartModal() {
+    this.isAddPartModalOpen = true;
+  }
+
+  closeAddPartModal() {
+    this.isAddPartModalOpen = false;
   }
 }
